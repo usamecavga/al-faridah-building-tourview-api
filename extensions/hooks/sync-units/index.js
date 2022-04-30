03e5ad810f4572e04ecdde8c6e81a4ce48e17d5d
Usame Cavga <>
2022-05-01 01:05:33 +0300

    Fixes

--- extensions/hooks/sync-units/index.js
+++ extensions/hooks/sync-units/index.js
@@ -1,7 +1,7 @@
 
 const axios = require('axios');
 module.exports = ({ schedule }) => {
-    schedule('56 21 * * *', async () => {
+    schedule('0,30 0,12 * * *', async () => {
         console.warn("schedule is runing");
 		await axios.get('https://xplor.alfaridah.ae/api/zoho/sync-units', { message: 'Job Done' });
 	});
